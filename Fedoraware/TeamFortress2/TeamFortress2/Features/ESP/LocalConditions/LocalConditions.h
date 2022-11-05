04390d84917649c074ac036054e06718a9d84681
stax <90155114+stax132349@users.noreply.github.com>
2022-11-04 21:12:20 -0700

    ok

---
+++ Fedoraware/TeamFortress2/TeamFortress2/Features/ESP/LocalConditions/LocalConditions.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "../../SDK/SDK.h"
+
+class LCESP
+{
+private:
+	static bool ShouldRun();
+	static const wchar_t* GetPlayerClass(int nClassNum);
+
+	void DrawPlayers(CBaseEntity* pLocal);
+	void DrawBuildings(CBaseEntity* pLocal) const;
+	void DrawWorld() const;
+	static void Draw3DBox(const Vec3* vPoints, Color_t clr);
+	static void CreateDLight(int nIndex, Color_t DrawColor, const Vec3& vOrigin, float flRadius);
+	static void DrawBones(CBaseEntity* pPlayer, const std::vector<int>& vecBones, Color_t clr);
+
+public:
+	void Run();
+	bool Argh();
+	std::vector<std::wstring> GetPlayerConditions(CBaseEntity* pEntity) const;
+	static bool GetDrawBounds(CBaseEntity* pEntity, Vec3* vTrans, int& x, int& y, int& w, int& h);
+};
+
+ADD_FEATURE(LCESP, LocalConditions)
