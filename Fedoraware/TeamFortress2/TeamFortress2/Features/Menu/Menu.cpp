9a52059e935b1f920b002432184805869727ce6b
stax <90155114+stax132349@users.noreply.github.com>
2022-12-30 16:28:20 -0800

    oopsies

--- Fedoraware/TeamFortress2/TeamFortress2/Features/Menu/Menu.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Features/Menu/Menu.cpp
@@ -1152,11 +1152,11 @@ void CMenu::MenuVisuals()
 						I::ViewRender->SetScreenOverlayMaterial(nullptr);
 					}
 					WToggle("Particle Colors", &Vars::Visuals::ParticleColors.Value);
+					ColorPickerL("Particle Color", Colors::ParticleColor);
 					if (Vars::Visuals::ParticleColors.Value)
 					{
 						WToggle("Rainbow Particles", &Vars::Visuals::RGBParticles.Value);
 					}
-					ColorPickerL("Particle Color", Colors::ParticleColor);
 					WToggle("Clean Screenshots", &Vars::Visuals::CleanScreenshots.Value);
 					WToggle("Crosshair aim position", &Vars::Visuals::CrosshairAimPos.Value);
 					WToggle("Box aim position", &Vars::Visuals::AimPosSquare.Value);
