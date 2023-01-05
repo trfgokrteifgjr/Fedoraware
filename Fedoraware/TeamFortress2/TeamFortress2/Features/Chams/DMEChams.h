8c60e7362a9054047563a11e4f3c0c149451ec07
stax <90155114+stax132349@users.noreply.github.com>
2023-01-04 17:16:16 -0800

    forgot

--- Fedoraware/TeamFortress2/TeamFortress2/Features/Chams/DMEChams.h
+++ Fedoraware/TeamFortress2/TeamFortress2/Features/Chams/DMEChams.h
@@ -25,7 +25,7 @@ public:
 	void CreateMaterials();
 	void DeleteMaterials();
 	IMaterial* CreateNRef(char const* szName, void* pKV, bool bSave = true);
-//	bool Render(const DrawModelState_t& pState, const ModelRenderInfo_t& pInfo, matrix3x4* pBoneToWorld);
+	bool Render(const DrawModelState_t& pState, const ModelRenderInfo_t& pInfo, matrix3x4* pBoneToWorld);
 	bool m_bRendering;
 	std::vector<IMaterial*> v_MatList;			//	cough
 	std::vector<IMaterial*> v_MatListGlobal;	//	this is for all materials. (I should order material creation and put it elsewhere)
