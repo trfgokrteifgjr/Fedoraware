7437290fead68b1230dee54d12cc313af2c93584
stax <90155114+stax132349@users.noreply.github.com>
2023-02-20 04:41:38 -0800

    fuck this cheat!!!!

--- Fedoraware/TeamFortress2/TeamFortress2/Features/Menu/ConfigManager/ConfigManager.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Features/Menu/ConfigManager/ConfigManager.cpp
@@ -1239,6 +1239,7 @@ bool CConfigManager::SaveVisual(const std::string& configName)
 		SAVE_VAR(Vars::Visuals::Particles::Feet::DashOnly);
 		SAVE_VAR(Vars::Visuals::BulletTracer);
 		SAVE_VAR(Vars::Visuals::BulletTracerRainbow);
+		SAVE_STRING(Vars::Visuals::ParticleName);
 		SAVE_VAR(Vars::Visuals::AimbotViewmodel);
 		SAVE_VAR(Vars::Visuals::ViewmodelSway);
 		SAVE_VAR(Vars::Visuals::ViewmodelSwayInterp);
@@ -1634,6 +1635,7 @@ bool CConfigManager::LoadVisual(const std::string& configName)
 		LOAD_VAR(Vars::Visuals::ParticleTracer);
 		LOAD_VAR(Vars::Visuals::BulletTracer);
 		LOAD_VAR(Vars::Visuals::BulletTracerRainbow);
+		LOAD_STRING(Vars::Visuals::ParticleName);
 		LOAD_VAR(Vars::Visuals::AimbotViewmodel);
 		LOAD_VAR(Vars::Visuals::ViewmodelSway);
 		LOAD_VAR(Vars::Visuals::ViewmodelSwayInterp);
