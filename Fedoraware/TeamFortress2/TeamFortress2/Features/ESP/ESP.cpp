b4c7884515867effeeb4932c302757f6e83f25c5
stax <90155114+stax132349@users.noreply.github.com>
2023-03-05 12:58:38 -0800

    Ping warning

--- Fedoraware/TeamFortress2/TeamFortress2/Features/ESP/ESP.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Features/ESP/ESP.cpp
@@ -576,6 +576,16 @@ void CESP::DrawPlayers(CBaseEntity* pLocal)
 				int offset = g_Draw.m_vecFonts[FONT].nTall / 4;
 				std::vector<std::wstring> cond_strings = GetPlayerConds(Player);
 
+				int ping = cResource->GetPing(Player->GetIndex());
+				if (Player != pLocal) // no reason to draw on local player
+				{
+					if (ping >= 200 || ping <= 10) // ping warning
+					{
+						g_Draw.String(FONT_ESP_COND, nTextX, y + nTextOffset, { 255, 95, 95, 255 }, ALIGN_DEFAULT, "%dMS", ping); //make it all caps so it matches with the condition esp
+						nTextOffset += g_Draw.m_vecFonts[FONT_ESP_COND].nTall;
+					}
+				}
+
 				if (!cond_strings.empty())
 				{
 					for (auto& condString : cond_strings)
