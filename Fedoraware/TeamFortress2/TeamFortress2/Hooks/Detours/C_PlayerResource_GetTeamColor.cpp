1352c88d948ea132d584c6e78d10eb5b0ffb317d
stax <90155114+stax132349@users.noreply.github.com>
2023-04-18 18:19:46 -0700

    Update C_PlayerResource_GetTeamColor.cpp

--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/C_PlayerResource_GetTeamColor.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/C_PlayerResource_GetTeamColor.cpp
@@ -3,6 +3,31 @@
 int iCurPlayer;
 unsigned char _color[4];
 
+__inline Color_t GetScoreboardColor(CBaseEntity* pEntity, bool enableOtherColors)
+{
+    Color_t out = Utils::GetTeamColor(pEntity->GetTeamNum(), enableOtherColors);
+    PlayerInfo_t info{}; I::EngineClient->GetPlayerInfo(pEntity->GetIndex(), &info);
+
+    if (pEntity->IsPlayer())
+    {
+        if (g_EntityCache.GetLocal()->GetIndex() == pEntity->GetIndex())
+        {
+            out = Colors::Local;
+        }
+
+        else if (g_EntityCache.IsFriend(pEntity->GetIndex()) || pEntity == g_EntityCache.GetLocal())
+        {
+            out = Colors::Friend;
+        }
+
+        else if (G::IsIgnored(info.friendsID))
+        {
+            out = Colors::Ignored;
+        }
+    }
+    return out;
+}
+
 MAKE_HOOK(C_TFPlayer_Resource_GetPlayerConnectionState, g_Pattern.Find(L"client.dll", L"55 8B EC 8B 45 ? 85 C0 74 ? 83 F8 ? 7F"), MM_PlayerConnectionState_t, __fastcall,
     void* ecx, void* edx, int iIndex)
 {
@@ -38,7 +63,7 @@ MAKE_HOOK(C_PlayerResource_GetTeamColor, g_Pattern.Find(L"client.dll", L"55 8B E
     CBaseEntity* ent = I::ClientEntityList->GetClientEntity(iCurPlayer);
     if (!ent || !ent->IsAlive() || !ent->IsPlayer()) { return Hook.Original<FN>()(ecx, edx, index); }
 
-    const Color_t cReturn = Utils::GetEntityDrawColor(ent, Vars::ESP::Main::EnableTeamEnemyColors.Value);
+    const Color_t cReturn = GetScoreboardColor(ent, Vars::ESP::Main::EnableTeamEnemyColors.Value);
 
     _color[0] = cReturn.r; _color[1] = cReturn.g; _color[2] = cReturn.b; _color[3] = 255;    //  force alpha
     return _color;
